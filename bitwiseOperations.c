37523f783c369aae6cc3ed803f81705cb696942f
mjkeller <michaelkeller1997@gmail.com>
2024-12-07 12:46:00 -0500

    bitshift example

--- bitwiseOperations.c
+++ bitwiseOperations.c
@@ -2,10 +2,13 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void main(){
-    uint8_t i = 0x01;
-    uint8_t j = 0x05;
-    uint8_t k;
-    k = i & j;
-    printf("0x%02x \n",k);
+void main(void){
+    /* Following along with https://youtu.be/ciio80nkjB8?si=kcyRGW0jXShW8r4A */
+    /* How to access a single bit */
+
+    uint32_t x = 42;    /* 101010 or 0x2a*/
+    printf("x = %d \n", x);
+
+    printf("bit 3 of x = %d \n", (x>>3) & 0x1); /*Shift bits right 3 */
+    printf("bit 4 of x = %d \n", (x>>4) & 0x1);
 }

75a780de9d418404b934fd3e79b5d37d719cee93
mjkeller <michaelkeller1997@gmail.com>
2024-12-07 12:52:09 -0500

    standardized for different shift values

--- bitwiseOperations.c
+++ bitwiseOperations.c
@@ -2,13 +2,12 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void main(void){
-    /* Following along with https://youtu.be/ciio80nkjB8?si=kcyRGW0jXShW8r4A */
-    /* How to access a single bit */
+#define BITVALUE(X, N) (((X) >> (N)) & 0x1)
 
+void main(void){
     uint32_t x = 42;    /* 101010 or 0x2a*/
     printf("x = %d \n", x);
 
-    printf("bit 3 of x = %d \n", (x>>3) & 0x1); /*Shift bits right 3 */
-    printf("bit 4 of x = %d \n", (x>>4) & 0x1);
+    printf("bit 3 of x = %d \n", BITVALUE(x,3)); /*Shift bits right 3 */
+    printf("bit 4 of x = %d \n", BITVALUE(x,3));
 }

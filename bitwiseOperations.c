a345fe91126a1c0c74c8b6a7915a88af317f474a
mjkeller <michaelkeller1997@gmail.com>
2024-12-08 11:37:47 -0500

    Printing out every bit

--- bitwiseOperations.c
+++ bitwiseOperations.c
@@ -8,6 +8,17 @@ void main(void){
     uint32_t x = 42;    /* 101010 or 0x2a*/
     printf("x = %d \n", x);
 
-    printf("bit 3 of x = %d \n", BITVALUE(x,3)); /*Shift bits right 3 */
-    printf("bit 4 of x = %d \n", BITVALUE(x,3));
+    int numBits = sizeof(x) * 8;
+    for(uint32_t i = 0; i < numBits; i++){
+        printf("%d",BITVALUE(x,(numBits-1)-i));
+    }
+    printf("\n");
+    printf("You see a lot of leading zeros in this because we are working with a 32 bit number\n");
+
+    uint8_t y = 42;
+    numBits = sizeof(y) * 8;
+    for(uint32_t i = 0; i < numBits; i++){
+        printf("%d",BITVALUE(y,(numBits-1)-i));
+    }
+    printf("\n");
 }
